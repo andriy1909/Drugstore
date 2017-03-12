c237c8074c73f60d2d484b107e22246856cdedeb
Alexander1998007 <invincible.ghost@yandex.ru>
2017-03-12 18:39:39 +0200

    Друк чеку фікс

--- Drugstore/Forms/SellGoodsForm.cs
+++ Drugstore/Forms/SellGoodsForm.cs
@@ -70,8 +70,8 @@ namespace Drugstore.Forms
         {
 
             int total = 0;
-           // float cash = float.Parse(txtCash.Text.Substring(1, 5));
-           // float change = 0.00f;
+            // float cash = float.Parse(txtCash.Text.Substring(1, 5));
+            // float change = 0.00f;
             //this prints the reciept
             Graphics graphic = e.Graphics;
             Font font = new Font("Courier New", 12); //must use a mono spaced font as the spaces need to line up
@@ -81,7 +81,7 @@ namespace Drugstore.Forms
             int startY = 10;
             int offset = 40;
 
-            graphic.DrawString("           DRUGSTORE", new Font("Courier New", 18), new SolidBrush(Color.Black), startX, startY);
+            graphic.DrawString("      DRUGSTORE", new Font("Courier New", 18), new SolidBrush(Color.Black), startX, startY);
             string top = "Item Name".PadRight(30) + "Price";
             graphic.DrawString(top, font, new SolidBrush(Color.Black), startX, startY + offset);
             offset = offset + (int)fontHeight; //make the spacing consistent
@@ -123,14 +123,22 @@ namespace Drugstore.Forms
 
                 totalprice += productPrice;
 
-               // if (productDescription.Contains("  -"))
-               // {
+                // if (productDescription.Contains("  -"))
+                // {
+                string productPriceOut = string.Format("{0:0.00}", productPrice);
                 string productLine = productDescription;
+                graphic.DrawString(productLine + "            " + productPriceOut, new Font("Courier New", 12, FontStyle.Italic), new SolidBrush(Color.Red), startX, startY + offset);
+                offset = offset + (int)fontHeight + 5; //make the spacing consistent
+
+                // graphic.DrawString(productPriceOut, new Font("Courier New", 12, FontStyle.Italic), new SolidBrush(Color.Red), startX, startY + offset);
+                // offset = offset + (int)fontHeight + 5; //make the spacing consistent
+
+             /* string productLine = productDescription;
                 graphic.DrawString(productLine, font, new SolidBrush(Color.Black), startX, 0);
                 string productPriceOut = productPrice.ToString().Trim();
                 graphic.DrawString(productPriceOut, font, new SolidBrush(Color.Black), startX, startY + offset);
                 offset = offset + (int)fontHeight + 5; //make the spacing consistent
-               /* }
+                }
                 else
                 {
                     string productLine = productDescription;
@@ -145,16 +153,16 @@ namespace Drugstore.Forms
             // change = (cash - totalprice);
             // when we have drawn all of the items add the total
             offset = offset + 20; //make some room so that the total stands out.
-            graphic.DrawString("Total to pay ".PadRight(30) + String.Format("{0:c}", totalprice), new Font("Courier New", 12, FontStyle.Bold), new SolidBrush(Color.Black), startX, startY + offset);
+            graphic.DrawString("Всього ".PadRight(30) + String.Format("{0:c}", totalprice), new Font("Courier New", 12, FontStyle.Bold), new SolidBrush(Color.Black), startX, startY + offset);
 
             // offset = offset + 30; //make some room so that the total stands out.
             // graphic.DrawString("CASH ".PadRight(30) + String.Format("{0:c}", cash), font, new SolidBrush(Color.Black), startX, startY + offset);
             // offset = offset + 15;
             // graphic.DrawString("CHANGE ".PadRight(30) + String.Format("{0:c}", change), font, new SolidBrush(Color.Black), startX, startY + offset);
             offset = offset + 30; //make some room so that the total stands out.
-            graphic.DrawString("     Thank-you for your custom,", font, new SolidBrush(Color.Black), startX, startY + offset);
+            graphic.DrawString("       Дякую за покупку,", font, new SolidBrush(Color.Black), startX, startY + offset);
             offset = offset + 15;
-            graphic.DrawString("       recovering faster)", font, new SolidBrush(Color.Black), startX, startY + offset);
+            graphic.DrawString("       Одужуйте швидше;)", font, new SolidBrush(Color.Black), startX, startY + offset);
         }
 
         private void button1_Click(object sender, EventArgs e)

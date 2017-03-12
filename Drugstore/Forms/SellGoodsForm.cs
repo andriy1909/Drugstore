a7254cff23e06f0cdb6ab63358781ae7c972e8b4
Alexander1998007 <invincible.ghost@yandex.ru>
2017-03-12 17:54:38 +0200

    Друк чеку
    Посуті має робити)

--- Drugstore/Forms/SellGoodsForm.cs
+++ Drugstore/Forms/SellGoodsForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Drawing.Printing;
 
 namespace Drugstore.Forms
 {
@@ -65,20 +66,124 @@ namespace Drugstore.Forms
             this.товариTableAdapter.Fill(this.drugstoreDataSet.Товари);
             товариBindingSource.Sort = "Назва DESC";
         }
+        public void CreateReceipt(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+            int total = 0;
+           // float cash = float.Parse(txtCash.Text.Substring(1, 5));
+           // float change = 0.00f;
+            //this prints the reciept
+            Graphics graphic = e.Graphics;
+            Font font = new Font("Courier New", 12); //must use a mono spaced font as the spaces need to line up
+            float fontHeight = font.GetHeight();
+
+            int startX = 10;
+            int startY = 10;
+            int offset = 40;
+
+            graphic.DrawString("           DRUGSTORE", new Font("Courier New", 18), new SolidBrush(Color.Black), startX, startY);
+            string top = "Item Name".PadRight(30) + "Price";
+            graphic.DrawString(top, font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + (int)fontHeight; //make the spacing consistent
+            graphic.DrawString("----------------------------------", font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + (int)fontHeight + 5; //make the spacing consistent
+
+            decimal totalprice = 0;
+
+          /*  foreach (string item in listBox1.Items)
+            {
+                //create the string to print on the reciept
+                string productDescription = item;
+                string productTotal = item.Substring(item.Length - 6, 6);
+                float productPrice = float.Parse(item.Substring(item.Length - 5, 5));
+
+                //MessageBox.Show(item.Substring(item.Length - 5, 5) + "PROD TOTAL: " + productTotal);
+
+                totalprice += productPrice;
+                if (productDescription.Contains("  -"))
+                {
+                    string productLine = productDescription.Substring(0, 24);
+                    graphic.DrawString(productLine, new Font("Courier New", 12, FontStyle.Italic), new SolidBrush(Color.Red), startX, startY + offset);
+                    offset = offset + (int)fontHeight + 5; //make the spacing consistent
+                }
+                else
+                {
+                    string productLine = productDescription;
+                    graphic.DrawString(productLine, font, new SolidBrush(Color.Black), startX, startY + offset);
+                    offset = offset + (int)fontHeight + 5; //make the spacing consistent
+                }
+            }
+           */
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                string productDescription = dataGridView1.Rows[i].Cells[0].Value.ToString().Trim();
+                string productTotal = dataGridView1.Rows[i].Cells[1].Value.ToString().Trim();
+                decimal productPrice = Convert.ToDecimal(dataGridView1.Rows[i].Cells[2].Value.ToString().Trim());
+
+                totalprice += productPrice;
+
+               // if (productDescription.Contains("  -"))
+               // {
+                string productLine = productDescription;
+                graphic.DrawString(productLine, font, new SolidBrush(Color.Black), startX, 0);
+                string productPriceOut = productPrice.ToString().Trim();
+                graphic.DrawString(productPriceOut, font, new SolidBrush(Color.Black), startX, startY + offset);
+                offset = offset + (int)fontHeight + 5; //make the spacing consistent
+               /* }
+                else
+                {
+                    string productLine = productDescription;
+
+                    graphic.DrawString(productLine, font, new SolidBrush(Color.Black), startX, startY + offset);
+
+                    offset = offset + (int)fontHeight + 5; //make the spacing consistent
+                }
+                */   
+            }
+
+            // change = (cash - totalprice);
+            // when we have drawn all of the items add the total
+            offset = offset + 20; //make some room so that the total stands out.
+            graphic.DrawString("Total to pay ".PadRight(30) + String.Format("{0:c}", totalprice), new Font("Courier New", 12, FontStyle.Bold), new SolidBrush(Color.Black), startX, startY + offset);
+
+            // offset = offset + 30; //make some room so that the total stands out.
+            // graphic.DrawString("CASH ".PadRight(30) + String.Format("{0:c}", cash), font, new SolidBrush(Color.Black), startX, startY + offset);
+            // offset = offset + 15;
+            // graphic.DrawString("CHANGE ".PadRight(30) + String.Format("{0:c}", change), font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + 30; //make some room so that the total stands out.
+            graphic.DrawString("     Thank-you for your custom,", font, new SolidBrush(Color.Black), startX, startY + offset);
+            offset = offset + 15;
+            graphic.DrawString("       recovering faster)", font, new SolidBrush(Color.Black), startX, startY + offset);
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                dataGridView1.Rows.Clear();
                 MessageBox.Show("До сплати: " + label1.Text);
                 label1.Text = "0";
                 textBox1.Clear();
+
+                PrintDialog printDialog = new PrintDialog();
+                PrintDocument printDocument = new PrintDocument();
+                printDialog.Document = printDocument; //add the document to the dialog box...        
+                printDocument.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(CreateReceipt); //add an event handler that will do the printing
+                //on a till you will not want to ask the user where to print but this is fine for the test envoironment.
+                DialogResult result = printDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    printDocument.Print();
+                }
+
+                dataGridView1.Rows.Clear();
             }
             catch (Exception err)
             {
                 MessageBox.Show(err.Message);
             }
+          
         }
 
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
